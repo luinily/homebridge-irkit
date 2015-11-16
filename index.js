288c00e4def79e2c253a9ea5a02afb8587ccb6c6
senyoltw <senyoltw@senyoltw-air.local>
2015-11-17 04:45:57 +0900

    form部分を変更

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ HttpAccessory.prototype = {
 		request({
 				url: url,
 				method: 'POST',
-				form: {message: form}
+				form: {form}
 			},
 			function(error, response, body) {
 				callback(error, response, body)
